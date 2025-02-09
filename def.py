cd93700d8cfc1390546b09383d278ab3b3023786
realbestia <94642708+realbestia@users.noreply.github.com>
2025-02-09 20:14:41 +0100

    def.py aktualisieren

--- def.py
+++ def.py
@@ -5,8 +5,8 @@ import os
 
 # Siti da cui scaricare i dati
 BASE_URLS = [
-    # "https://huhu.to",
-    "https://vavoo.to",
+    "https://huhu.to",
+    # "https://vavoo.to",
     # "https://kool.to",
     # "https://oha.to"
 ]
