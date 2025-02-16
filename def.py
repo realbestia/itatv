8fabe65c61ca5c4fd29e35787ed93dd4ce4501da
realbestia <94642708+realbestia@users.noreply.github.com>
2025-02-16 02:55:38 +0100

    def.py aktualisieren

--- def.py
+++ def.py
@@ -137,7 +137,7 @@ def get_tvg_id_from_epg(tvg_name, epg_data):
                 best_score = similarity
                 best_match = channel.get("id")
 
-            if best_score >= 95:
+            if best_score >= 99:
                 return best_match
 
     return best_match if best_score >= 80 else ""
