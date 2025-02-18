f488848b55eaf23172255e2756c716dcabbed986
realbestia <94642708+realbestia@users.noreply.github.com>
2025-02-18 19:17:22 +0100

    Update epg.py

--- epg.py
+++ epg.py
@@ -3,23 +3,39 @@ import gzip
 import shutil
 import os
 
-# URL del file gzip
-url = 'https://www.epgitalia.tv/gzip'
+# URL del file .gzip e del file XML
+url_gzip = 'https://www.epgitalia.tv/gzip'
+url_xml = 'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/it.xml'
 
-# Nome del file dove salvare il file scaricato
-output_filename = 'file_scaricato.gz'
+# Nome del file dove salvare il file scaricato .gzip
+output_gzip_filename = 'file_scaricato.gz'
 
-# Scaricare il file
-response = requests.get(url)
-with open(output_filename, 'wb') as file:
-    file.write(response.content)
+# Scaricare il file .gzip
+response_gzip = requests.get(url_gzip)
+with open(output_gzip_filename, 'wb') as file:
+    file.write(response_gzip.content)
 
 # Decomprimere il file .gz e salvarlo come epg.xml
-with gzip.open(output_filename, 'rb') as f_in:
+with gzip.open(output_gzip_filename, 'rb') as f_in:
     with open('epg.xml', 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
 
 # Eliminare il file .gz
-os.remove(output_filename)
+os.remove(output_gzip_filename)
 
-print('Download, decompressione e eliminazione del file .gz completati.')
+# Scaricare il file XML aggiuntivo
+response_xml = requests.get(url_xml)
+with open('plutoTV.xml', 'wb') as file:
+    file.write(response_xml.content)
+
+# Unire i contenuti dei due file XML
+with open('epg.xml', 'rb') as file1, open('plutoTV.xml', 'rb') as file2:
+    with open('epg_unito.xml', 'wb') as output_file:
+        shutil.copyfileobj(file1, output_file)
+        shutil.copyfileobj(file2, output_file)
+
+# Eliminare i file XML separati dopo aver unito i contenuti
+os.remove('epg.xml')
+os.remove('plutoTV.xml')
+
+print('Download, decompressione, unione dei file e pulizia completati.')
