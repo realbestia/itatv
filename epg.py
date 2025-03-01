2a4fe81ba34bdb78af9d736301a3d7aed457ac04
realbestia <94642708+realbestia@users.noreply.github.com>
2025-03-01 22:24:37 +0100

    Update epg.py

--- epg.py
+++ epg.py
@@ -173,6 +173,14 @@ def main():
         italian_channels = filter_italian_channels(channels, url)
         all_links.extend(italian_channels)
 
+    # Aggiungi manualmente il canale DAZN1
+    dazn1_url = "https://daddylive.mp/embed/stream-877.php"
+    dazn1_name = "DAZN1"
+    dazn1_base_url = "https://daddylive.mp"
+    service, category = classify_channel(dazn1_name)  # Usa la funzione di classificazione
+    all_links.append((dazn1_name, dazn1_url, dazn1_base_url))
+
+    # Organizza i canali
     organized_channels = {service: {category: [] for category in ["Sport", "Film & Serie TV", "News", "Altro", "Rai", "Mediaset", "Bambini", "Documentari", "Musica"]} for service in ["Sky", "DTT", "IPTV gratuite"]}
     
     for name, url, base_url in all_links:
