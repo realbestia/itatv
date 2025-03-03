c3edc0f06c6550f7ce86436e9b64bd7e0afcdff4
realbestia <94642708+realbestia@users.noreply.github.com>
2025-03-03 04:27:37 +0100

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -17,7 +17,7 @@ EPG_OUTPUT_FILE = "itaevents.xml"
 LOGO = "https://raw.githubusercontent.com/realbestia/itatv/refs/heads/main/logo.png"
 
 # Define keywords for filtering channels
-EVENT_KEYWORDS = ["italy", "atp", "formula uno", "f1", "motogp", "moto gp"]
+EVENT_KEYWORDS = ["italy", "formula uno", "f1", "motogp", "moto gp"]
 
 mStartTime = 0
 mStopTime = 0
