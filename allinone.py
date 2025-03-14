ed19e073340707ce93d66b0b5c1bd941ea952a44
realbestia <94642708+realbestia@users.noreply.github.com>
2025-03-14 19:08:57 +0100

    Update allinone.py

--- allinone.py
+++ allinone.py
@@ -13,13 +13,12 @@ def download_playlist(url, exclude_x_tvg_url=False, remove_extm3u=False, add_tvg
     playlist = response.text
     
     if append_params:
-        # Se siamo nella playlist eventi.m3u8, aggiungi i parametri
+        # Aggiungi i parametri agli URL di streaming nella playlist
         playlist_lines = playlist.splitlines()
         for i in range(len(playlist_lines)):
             if '.m3u8' in playlist_lines[i]:  # Cerca i link .m3u8
-                if "eventi.m3u8" in playlist_lines[i]:  # Se è il link giusto
-                    # Aggiungi i parametri alla fine del link
-                    pass  # Non facciamo più nulla qui, dato che dobbiamo rimuovere extra_params
+                # Aggiungi i parametri alla fine del link
+                playlist_lines[i] += "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Filovetoplay.xyz%2F&h_origin=https%3A%2F%2Filovetoplay.xyz"
         playlist = '\n'.join(playlist_lines)
     
     if exclude_x_tvg_url:
@@ -47,7 +46,7 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Scarica le playlist, escludendo 'x-tvg-url=' dalla terza playlist, rimuovendo la riga '#EXTM3U' e aggiungendo 'tvg-url'
 playlist1 = download_playlist(url1)
-playlist2 = download_playlist(url2)
+playlist2 = download_playlist(url2, append_params=True)  # Aggiungi i parametri alla playlist eventi.m3u8
 playlist3 = download_playlist(url3, exclude_x_tvg_url=True, remove_extm3u=True, add_tvg_url=True)
 
 # Unisci le tre playlist
