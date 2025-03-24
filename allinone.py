2b1e52a059297ab4d0f8c0d85af940585e379a2e
realbestia <94642708+realbestia@users.noreply.github.com>
2025-03-24 01:33:03 +0100

    Update allinone.py

--- allinone.py
+++ allinone.py
@@ -8,7 +8,7 @@ url3 = "https://raw.githubusercontent.com/Brenders/Pluto-TV-Italia-M3U/main/Plut
 url4 = "https://mfp2.nzo66.com/proxy/hls/manifest.m3u8?api_password=mfp123&d=https://raw.githubusercontent.com/realbestia/itatv/refs/heads/main/world.m3u8"
 
 # Funzione per scaricare una playlist
-def download_playlist(url, exclude_x_tvg_url=False, remove_extm3u=False, add_tvg_url=False, append_params=False, exclude_group_title=None):
+def download_playlist(url, remove_extm3u=False, append_params=False, exclude_group_title=None):
     response = requests.get(url)
     response.raise_for_status()  # Se c'è un errore, solleva un'eccezione
     playlist = response.text
@@ -22,27 +22,16 @@ def download_playlist(url, exclude_x_tvg_url=False, remove_extm3u=False, add_tvg
                 playlist_lines[i] += "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Filovetoplay.xyz%2F&h_origin=https%3A%2F%2Filovetoplay.xyz"
         playlist = '\n'.join(playlist_lines)
     
-    if exclude_x_tvg_url:
-        # Filtra la linea contenente 'x-tvg-url='
-        playlist = '\n'.join(line for line in playlist.split('\n') if not line.startswith('x-tvg-url='))
-    
     if remove_extm3u:
         # Rimuovi la riga che inizia con '#EXTM3U'
         playlist = '\n'.join(line for line in playlist.split('\n') if not line.startswith('#EXTM3U'))
     
-    if add_tvg_url:
-        # Aggiungi 'tvg-url' subito dopo 'group-title="Pluto TV Italia"'
-        lines = playlist.split('\n')
-        for i in range(len(lines)):
-            if 'group-title="Pluto TV Italia"' in lines[i]:
-                # Trova l'indice del group-title e aggiungi l'tvg-url subito dopo
-                index = lines[i].find('group-title="Pluto TV Italia"') + len('group-title="Pluto TV Italia"')
-                lines[i] = lines[i][:index] + ' tvg-url="https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/it.xml"' + lines[i][index:]
-        playlist = '\n'.join(lines)
-    
     # Escludi canali con un determinato group-title
     if exclude_group_title:
         playlist = '\n'.join(line for line in playlist.split('\n') if exclude_group_title not in line)
+    
+    # Aggiungi il nuovo #EXTM3U tvg-url all'inizio della playlist
+    playlist = '#EXTM3U tvg-url="https://raw.githubusercontent.com/realbestia/itatv/refs/heads/main/epg.xml"\n' + playlist
     
     return playlist
 
@@ -52,7 +41,7 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 # Scarica le playlist, escludendo 'x-tvg-url=' dalla terza playlist, rimuovendo la riga '#EXTM3U' e aggiungendo 'tvg-url'
 playlist1 = download_playlist(url1)
 playlist2 = download_playlist(url2, append_params=True)  # Aggiungi i parametri alla playlist eventi.m3u8
-playlist3 = download_playlist(url3, exclude_x_tvg_url=True, remove_extm3u=True, add_tvg_url=True)
+playlist3 = download_playlist(url3, remove_extm3u=True)
 playlist4 = download_playlist(url4, exclude_group_title="Italy")  # Escludi i canali con group-title="Italy"
 
 # Unisci le quattro playlist
