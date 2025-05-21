6db1c6eccb12c5102fa0cff1d3eb3d4201b593ab
realbestia <94642708+realbestia@users.noreply.github.com>
2025-05-21 03:41:58 +0200

    listaMFP.py aktualisieren

--- listaMFP.py
+++ listaMFP.py
@@ -229,7 +229,15 @@ def eventi_m3u8_generator():
             if teams_match:
                 team1, team2 = teams_match.groups()
                 search_queries = [
-                    f"{team1} vs {team2} logo dazn"
+                    f"{team1} vs {team2} logo dazn",
+                    f"{team1} {team2} partita logo",
+                    f"{team1} {team2} logo evento sportivo",
+                    f"{team1} vs {team2} match logo",
+                    f"{clean_event_name} logo",
+                    f"{team1} vs {team2} hd logo",
+                    f"{team1} vs {team2} tv logo",
+                    f"{team1} vs {team2} sky dazn logo",
+                    f"{team1} vs {team2} streaming logo",
                 ]
             else:
                 search_queries = [
